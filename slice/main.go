bea83394be4dfda5324f5eda8b4e468287bc76ed
rahul patil <rrp30998@gmail.com>
2024-06-27 20:54:49 +0530

    array, slice, error hamdling

---
+++ slice/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 4, 5}
+	// numbers = append(numbers, 1, 5, 6, 7, 8, 9)
+	// fmt.Println(numbers)
+	// fmt.Printf("data type of silce %T\n", numbers)
+	// fmt.Println("leng of slice", len(numbers))
+
+	fmt.Println("slice:", numbers)
+	fmt.Println("len:", len(numbers))
+	fmt.Println("capacity:", cap(numbers))
+}
